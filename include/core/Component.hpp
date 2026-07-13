699ed7ece81d208f3443e80b3d910b057c723ec3
Julian Konowalski <julian.konwalski@gmail.com>
2026-07-13 16:51:02 +0200

    move component template method definitions outside of class declaration

--- include/core/Component.hpp
+++ include/core/Component.hpp
@@ -21,15 +21,20 @@ class Component {
     inline Entity& get_parent(void) { return m_parent; }
 
     template <typename T>
-    static size_t get_type_id(void) {
-        static_assert(std::is_base_of_v<Component, T>,
-                      "T must derive from vq::core::Component class");
-        return TypeId<Component>::get_type_id<T>();
-    }
+    static size_t get_type_id(void);
 
   protected:
     Entity& m_parent;
 };
+
+/*----------------------------------------------------------------------------*/
+
+template <typename T>
+size_t Component::get_type_id(void) {
+    static_assert(std::is_base_of_v<Component, T>,
+                  "T must derive from vq::core::Component class");
+    return TypeId<Component>::get_type_id<T>();
+}
 
 /*----------------------------------------------------------------------------*/
 
