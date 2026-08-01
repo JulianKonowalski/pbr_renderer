7b0a9be0380a47dac8cecac59d84e6933209e351
JulianKonowalski <julian.konwalski@gmail.com>
2026-08-01 10:44:04 +0200

    split EventEmitter into hpp and cpp files

--- include/core/EventEmitter.hpp
+++ include/core/EventEmitter.hpp
@@ -14,28 +14,13 @@ class EventEmitter {
     EventEmitter(void)  = default;
     ~EventEmitter(void) = default;
 
-    void attach_handler(EventHandler& event_handler) {
+    void detach_handler(EventHandler& event_handler);
+    inline void attach_handler(EventHandler& event_handler) {
         m_event_handlers.push_back(&event_handler);
     }
 
-    void detach_handler(EventHandler& event_handler) {
-        auto event_handler_it = std::find_if(
-            m_event_handlers.begin(), m_event_handlers.end(),
-            [&](auto* handler) { return handler == &event_handler; });
-        if (event_handler_it != m_event_handlers.end()) {
-            m_event_handlers.erase(event_handler_it);
-        }
-    }
-
   protected:
-    void emit_event(Event& event) {
-        for (auto* event_handler : m_event_handlers) {
-            event_handler->handle(event);
-            if (event.is_handled()) {
-                break;
-            }
-        }
-    }
+    void emit_event(Event& event);
 
   private:
     std::vector<EventHandler*> m_event_handlers;
