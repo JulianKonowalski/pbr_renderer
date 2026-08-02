c6dcd7c0ff5b3b5ac5e8ab34a54180a00502998d
JulianKonowalski <julian.konwalski@gmail.com>
2026-08-02 20:39:25 +0200

    add examples directory

---
+++ examples/main.cpp
@@ -0,0 +1,26 @@
+#include <io/Window.hpp>
+
+class WindowCloseHandler : public vq::core::EventHandler {
+  public:
+    WindowCloseHandler()           = default;
+    ~WindowCloseHandler() override = default;
+
+    void handle(vq::core::Event& event) override {
+        if (auto p_casted = dynamic_cast<vq::io::KeyEvent*>(&event)) {
+            if (p_casted->key == 256) { // GLFW_KEY_ESCAPE
+                p_casted->window.close();
+                p_casted->set_handled();
+            }
+        }
+    }
+};
+
+int main(void) {
+    vq::io::Window window(vq::io::Window::WindowSpecification("VQ Example"));
+    WindowCloseHandler close_handler;
+    window.attach_event_handler(close_handler);
+
+    while (!window.should_close()) {
+        window.update();
+    }
+}
